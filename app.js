176b1c9fa6e457c2c6d54dd063ba89a259f43609
rysntkang <132887121+rysntkang@users.noreply.github.com>
2024-12-04 12:50:36 +0800

    Skeleton Layout for website

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Set up EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files (CSS, JS, Images)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Features' , page: 'features'});
+  });
+  
+app.get('/pricing', (req, res) => {
+    res.render('index', { title: 'Pricing', page: 'pricing' });
+});
+
+app.get('/about', (req, res) => {
+    res.render('index', { title: 'About', page: 'about' });
+});
+
+  
+  
+  
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
